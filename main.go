3168bd17416ef2075587c6417f1aa455cadc3f0e
Enes Karali <eneskarali96@hotmail.com>
2020-08-18 22:57:04 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
-package main
+package gopylos
 
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/oauth"
+	"gopylos/gopylos/collector"
+	"log"
+)
 
+var (
+	Key = ""
+)
 
-func main() {
-	
+var (
+	client collector.EventCollectorClient
+)
+
+func Start() error {
+	if len(Key) <= 0 {
+		return errors.New("access token should be initialized")
+	}
+
+	perRPC := oauth.NewOauthAccess(fetchToken(Key))
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	conn, err := grpc.Dial("localhost:8086", grpc.WithTransportCredentials(credentials.NewTLS(config)), grpc.WithPerRPCCredentials(perRPC))
+	if err != nil {
+		log.Fatalf("connection error: %v", err)
+		return errors.New("connection error")
+	}
+
+	client = collector.NewEventCollectorClient(conn)
+	return nil
+}
+func SendUsage(planId string, userId string, usage float32) error {
+	_, err := client.SendUsage(context.Background(), &collector.UsageRequest{PlanId: planId, UserId: userId, Usage: usage})
+	if err != nil {
+		log.Fatalf("send event error, %v", err)
+		return err
+	}
+	return nil
+}
+
+func fetchToken(key string) *oauth2.Token {
+	return &oauth2.Token{
+		AccessToken: key,
+	}
 }
